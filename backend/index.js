e25b6d19426168c9eeba5d00bfda5884eaaa1f07
thefakehamma <hamma.khoshnaw@gmail.com>
2025-03-05 09:52:43 +0100

    Fix package frontend and index backend

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,14 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("✅ Powerhouse Club API is running!");
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/recruitments", recruitmentRoutes);
 app.use("/api/contributions", contributionRoutes);
 app.use("/api/campaign", campaignRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
