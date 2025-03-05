9619129d26e33836297d68db29685420e43ceb72
thefakehamma <hamma.khoshnaw@gmail.com>
2025-03-05 10:05:11 +0100

    Fix package and vite one more time

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    outDir: "dist", // ✅ Ensure this is the correct directory
-    emptyOutDir: true, // ✅ Clean old builds before building
-  },
 });
